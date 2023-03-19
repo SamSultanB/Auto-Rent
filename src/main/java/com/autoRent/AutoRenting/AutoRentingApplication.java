7866c405a4681953b023ddbf42bcc087e648cae8
lux <ssbekmuratov@gmail.com>
2023-03-19 17:18:00 +0600

    first

---
+++ src/main/java/com/autoRent/AutoRenting/AutoRentingApplication.java
@@ -0,0 +1,13 @@
+package com.autoRent.AutoRenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoRentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoRent/AutoRenting/AutoRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoRent.AutoRenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
