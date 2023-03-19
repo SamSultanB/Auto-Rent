c38e1ecba46520ce7c62f1f38ec1d6e57cecc3cc
lux <ssbekmuratov@gmail.com>
2023-03-19 21:55:15 +0600

    entities

---
+++ src/main/java/com/autoRent/AutoRenting/models/Auto.java
@@ -0,0 +1,13 @@
+package com.autoRent.AutoRenting.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Auto {
+    private String registrationMark;
+    private String model;
+    private String color;
+    private String year;
+    private String volume;
+    private String passengerSeats;
+    private String pricePerDay;
+}
